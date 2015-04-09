074bbfc486b47236ef4a1fb6c1e152b4d5de2a03
Thomas Kristiansen <thom_lfc@hotmail.com>
2015-04-09 11:55:32 +0200

    Skrevet generelt og utviklingsverktøy

--- Kulturhus/src/Billett.java
+++ Kulturhus/src/Billett.java
@@ -1,11 +1,14 @@
 public class Billett
 {
+    private String fornavn, etternavn;
     private int plassNr, tlf, pris;
     private Lokale lokale;
     private Arrangement arr;
     
-    public Billett(int pN, int t, int p, Lokale l, Arrangement a)
+    public Billett(String fnavn, String enavn, int pN, int t, int p, Lokale l, Arrangement a)
     {
+        fornavn = fnavn;
+        etternavn = enavn;
         plassNr = pN;
         tlf = t;
         lokale = l;
